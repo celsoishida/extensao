969348e0b12f5d4f0d67b7267f8be17b4d691f80
Celso <celsoishida@gmail.com>
2017-05-31 16:36:18 -0300

    versão 0.1
    Primeira versão

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" /> 
+<meta name="description" content="extensão, UFPR"></meta>
+<meta name="keywords" content="Extensão"></meta>
+
+
+	<title>Extensão Universitária - UFPR</title>
+	<link rel="stylesheet" type="text/css" media="screen" href="../tutorial/style.css" />
+	<script src="../celsoishida/jquery.js"></script>
+
+</head>
+<body>
+<?php include_once("../tutorial/analyticstracking.php") ?>
+
+    <div id="wrap">
+        <div class="container">
+            
+			<!-- incluindo header-->
+			<?php include '../celsoishida/header.html'; ?>
+
+			
+            <div id="main">
+
+			<!-- incluindo links-->
+			<?php include '../tutorial/linksS.html'; ?>
+			
+
+                <div class="content">
+                    <!-- Coluna esquerda -->
+                    <div class="left nav-left">
+
+						<?php include 'menu_lateral.html'; ?>
+						
+						
+                    </div>
+                    <!-- Fim Coluna esquerda -->
+					
+                    <!-- Coluna direita -->
+                    <div class="right nav-right">
+					
+<h2 class="title">Extensão</h2>
+
+
+<h4></h4>
+<br />
+<br />
+<iframe src="https://www.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=eqfc0ecrief5iriavjog78a17g%40group.calendar.google.com&amp;color=%23853104&amp;ctz=America%2FSao_Paulo" style=" border-width:0 " width="600" height="400" frameborder="0" scrolling="no"></iframe>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+</div>
+                    <!-- Fim Coluna direita -->
+                    <p class="clear">
+                    </p>
+                </div>
+                <div class="content-base">
+                </div>
+            </div>
+            <!-- Fim main -->
+        </div>
+        <!-- Fim wrap -->
+    </div>
+    <!-- fim container -->
+    <div id="footer">
+		<!-- incluindo rodape-->
+		<?php include "../celsoishida/rodape.html"; ?>
+    </div>
+</body>
+</html>
+
---
+++ sqlmadi.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" /> 
+<meta name="description" content="SQL Server management studio"></meta>
+<meta name="keywords" content="SQL Server"></meta>
+
+
+	<title>Download and install SQL Server Management Studio for free</title>
+	<link rel="stylesheet" type="text/css" media="screen" href="../tutorial/style.css" />
+	<script src="../celsoishida/jquery.js"></script>
+
+</head>
+<body>
+<?php include_once("../tutorial/analyticstracking.php") ?>
+
+    <div id="wrap">
+        <div class="container">
+            
+			<!-- incluindo header-->
+			<?php include '../celsoishida/header.html'; ?>
+
+			
+            <div id="main">
+
+			<!-- incluindo links-->
+			<?php include '../tutorial/linksS.html'; ?>
+			
+
+                <div class="content">
+                    <!-- Coluna esquerda -->
+                    <!--div class="left nav-left">
+						
+                    </div>
+                    <!-- Fim Coluna esquerda -->
+					
+                    <!-- Coluna direita -->
+                    <div class="right nav-center">
+					
+<h2 class="title">Download and Install Microsoft SQL Management Studio express for free  <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a></h2>
+
+<p>Download and install SQL Server Management Studio for free.</p>
+<br />
+<table> <tr><td>
+<iframe width="600" height="400" src="https://www.youtube.com/embed/g7zPgEUH8o8?list=PL9M43qkmJ7uZ3r48guao0q6QzhUgLnF2u&autoplay=1" frameborder="0" allowfullscreen></iframe>
+</td><td> Se desejar, veja:
+<a href="https://www.youtube.com/watch?v=VkGy64GKVIQ&index=2&list=PL9M43qkmJ7uZ3r48guao0q6QzhUgLnF2u"><img width="200" height="140" src="moodle.png" /></a>
+<br /><br />
+<a  href="como.php"><img width="200" height="140" src="../images/dicasSQL.png" alt="Outras Dicas de SQL Server"  title="Outras Dicas de SQL Server" /></a>
+</td></tr></table>
+
+	<br /><script type="text/javascript"><!--
+google_ad_client = "pub-9038432961917402";
+/* 728x90, criado 05/09/11 celso cedsmi graduacao */
+google_ad_slot = "8529206886";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<br />
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/MovingImage" property="dct:title" rel="dct:type">Inserir diversos usuários no SQL Server</span> de <a xmlns:cc="http://creativecommons.org/ns#" href="https://www.youtube.com/embed/g7zPgEUH8o8?list=PLDDF6D5BACB387D42&autoplay=1" property="cc:attributionName" rel="cc:attributionURL">Celso Yoshikazu Ishida</a> está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons - Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.
+<br />
+
+</div>
+                    <!-- Fim Coluna direita -->
+                    <p class="clear">
+                    </p>
+                </div>
+                <div class="content-base">
+                </div>
+            </div>
+            <!-- Fim main -->
+        </div>
+        <!-- Fim wrap -->
+    </div>
+    <!-- fim container -->
+    <div id="footer">
+    </div>
+</body>
+</html>
+
